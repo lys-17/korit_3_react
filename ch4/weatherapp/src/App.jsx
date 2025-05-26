a13821aa1e8646dbd7db6fcbc9165dd6e7928322
ãJaehun3515 <jiro14@nate.com>
2025-05-26 11:22:39 +0900

    github api

--- ch4/weatherapp/src/App.jsx
+++ ch4/weatherapp/src/App.jsx
@@ -1,13 +1,38 @@
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [weather, setWeather] = useState ({
+    temp: '',
+    desc: '',
+    icon: '',
+  });
 
-  return (
-    <>
-      
-    </>
-  )
+  useEffect(() => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Busan&units=Metric&APIkey=1100fb9f2eda53f92625c834248c303f')
+    .then(response => response.json())
+    .then(result => {
+      setWeather({
+        temp:result.main.temp,
+        desc:result.weather[0].description,
+        icon:result.weather[0].icon,
+      });
+    })
+    .catch(err => console.log(err));
+  }, []);
+
+  if(weather.icon){
+    return (
+      <>
+        <p>온도 : {weather.temp}</p>
+        <p>설명 : {weather.desc}</p>
+        <p>아이콘 : {weather.icon}</p>
+        <img src={`http://openweathermap.org/img/wn ${weather.icon}@2x.png`} alt="날씨 아이콘입니다." />
+      </>
+    )
+  } else {
+    return <h1>로딩 중...</h1>
+  }
 }
 
 export default App
